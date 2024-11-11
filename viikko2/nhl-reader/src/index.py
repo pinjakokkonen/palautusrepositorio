6e15b4a6bc5e3e52635fb4619ac3777a56ade7ed
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2024-11-11 23:55:12 +0200

    viikko2 tehtävä 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,23 +1,39 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = requests.get(url).json()
+
+    def get_players(self):
+        self.players = []
+        for player_dict in self.url:
+            player = Player(player_dict)
+            self.players.append(player)
+        return self.players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        self.players_by_nationality = []
+        for player in self.players:
+            if player.nationality==nationality:
+                self.players_by_nationality.append(player)
+        self.sorted = sorted(self.players_by_nationality, key=lambda player: player.points, reverse=True)
+        return self.sorted
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     print("Players from FIN\n")
 
-    players.sort(key=lambda player: player.points, reverse=True)
-
     for player in players:
-        if player.nationality=="FIN":
-            print(player)
+        print(player)
 
 if __name__ == "__main__":
     main()
